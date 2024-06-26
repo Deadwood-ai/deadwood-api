19b29be86007bbdcae7e28d6ad0c71717482777b
Mirko Mälicke <mirko@hydrocode.de>
2024-06-26 17:02:09 +0200

    add settings

---
+++ src/settings.py
@@ -0,0 +1,48 @@
+from typing import Optional, Union, Literal
+from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+from pathlib import Path
+
+# load an .env file if it exists
+load_dotenv()
+
+
+BASE = str(Path(__file__).parent.parent.parent / "data")
+
+
+# load the settings from environment variables
+class Settings(BaseSettings):
+    # base directory for the storage app
+    base_dir: str = BASE
+
+    # supabase settings for supabase authentication
+    supabase_url: Optional[str] = None
+    supabase_key: Optional[str] = None
+
+    # some basic settings for the UVICORN server
+    uvicorn_host: str = "127.0.0.1"
+    uvicorn_port: int = 8000
+    uvicorn_root_path: str = "/"
+    uvicorn_proxy_headers: bool = True
+
+    # supabase settings
+    metadata_table: str = 'metadata'
+    processor_username: str = 'processor@deadtrees.earth'
+    processor_password: str = 'processor'
+
+    # SFTP settings
+    storage_ssh_user: Optional[str] = None
+    storage_ssh_host: Optional[str] = None
+    storage_ssh_password: Optional[str] = None
+
+
+    @property
+    def base_path(self) -> Path:
+        path = Path(self.base_dir)
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
+        
+        return path
+
+
+settings = Settings()
