a5fe0b9562b4a7e96082b415b824a11f2cc8e1e5
Mirko Mälicke <mirko@hydrocode.de>
2024-07-05 09:55:57 +0200

    fix broken supabase login

--- src/supabase.py
+++ src/supabase.py
@@ -2,7 +2,7 @@ from typing import Union, Generator, Literal, Optional
 from contextlib import contextmanager
 
 from supabase import create_client
-from supabase.client import Client
+from supabase.client import Client, ClientOptions
 from gotrue import User
 
 from .settings import settings
@@ -10,12 +10,13 @@ from .settings import settings
 
 def login(user: str, password: str):
     # create a supabase client
-    client = create_client(settings.supabase_url, settings.supabase_key)
+    client = create_client(settings.supabase_url, settings.supabase_key, options=ClientOptions(auto_refresh_token=False))
 
     client.auth.sign_in_with_password({'email': user, 'password': password})
     auth_response = client.auth.refresh_session()
     
-    client.auth.sign_out()
+    # client.auth.sign_out()
+    # client.auth.close()
     # return the response
     return auth_response
 
@@ -35,7 +36,7 @@ def verify_token(jwt: str) -> Union[Literal[False], User]:
 @contextmanager
 def use_client(access_token: Optional[str] = None) -> Generator[Client, None, None]:
     # create a supabase client
-    client = create_client(settings.supabase_url, settings.supabase_key)
+    client = create_client(settings.supabase_url, settings.supabase_key, options=ClientOptions(auto_refresh_token=False))
 
     # yield the client
     try:
@@ -45,4 +46,6 @@ def use_client(access_token: Optional[str] = None) -> Generator[Client, None, No
         
         yield client
     finally:
-        client.auth.sign_out()
+        #client.auth.sign_out()
+        # client.auth.close()
+        pass
