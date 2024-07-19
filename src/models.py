0b80c2846fb63fbd76c6c65d7aedb0163edf9a04
Mirko Mälicke <mirko.maelicke@kit.edu>
2024-07-19 13:00:57 +0200

    adding the route

--- src/models.py
+++ src/models.py
@@ -101,7 +101,9 @@ class Cog(BaseModel):
     created_at: Optional[datetime] = None
 
     @field_serializer('created_at', mode='plain')
-    def datetime_to_isoformat(field: datetime) -> str:
+    def datetime_to_isoformat(field: datetime | None) -> str | None:
+        if field is None:
+            return None
         return field.isoformat()
 
 
