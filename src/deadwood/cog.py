3f90e6abd8596481b1052c296f93ada41facf492
Mirko Mälicke <mirko.maelicke@kit.edu>
2024-07-19 10:19:18 +0200

    little bit of cleanup

--- src/deadwood/cog.py
+++ src/deadwood/cog.py
@@ -13,13 +13,13 @@ def calculate_cog(tiff_file_path, cog_target_path, profile="webp", overviews=Non
     config = dict(
         GDAL_NUM_THREADS="ALL_CPUS",
         GDAL_TIFF_INTERNAL_MASK=True,
-        GDAL_TIFF_OVR_BLOCKSIZE="128",
+        # GDAL_TIFF_OVR_BLOCKSIZE=f"{blocksize}",
     )
 
     cog_translate(tiff_file_path,
     cog_target_path,
     output_profile,
-    #config=config,
+    config=config,
     overview_level=overviews, # OGEO overview (decimation) level. By default, inferred from data size.
     #overview_resampling=, # RasterIO Resampling algorithm for overviews
     #zoom_level_strategy=, # Strategy to determine zoom level (same as in GDAL 3.2).
@@ -37,21 +37,13 @@ def calculate_cog(tiff_file_path, cog_target_path, profile="webp", overviews=Non
     #allow_intermediate_compression=, # Allow intermediate file compression to reduce memory/disk footprint.
         # Note: This could reduce the speed of the process.
     #additional_cog_metadata=, #  Additional dataset metadata to add to the COG.
-    #use_cog_driver=, # Use GDAL COG driver if set to True. COG driver is available starting with GDAL 3.1.
+    use_cog_driver=True, # Use GDAL COG driver if set to True. COG driver is available starting with GDAL 3.1.
     )
 
     if not cog_validate(cog_target_path):
-        # wenn die settings dazu führen könnten dass das cog kaputt ist(?):
-        raise
-    
-    # zum testen und nachvollziehen:
-    info_output_cog = cog_info(cog_target_path)
+        # check if the cog is valid
+        raise RuntimeError(f"Validation failed for {cog_target_path}")
 
-    print(info_output_cog['Profile'])
-    print(info_output_cog['GEO'])
-    print(info_output_cog['Tags'])
-    print(f'Anzahl overviews: {len(info_output_cog['IFD']) - 1}')
-    for level in info_output_cog['IFD']:
-        print(level)
-
+    # return info
+    return cog_info(cog_target_path)
 
