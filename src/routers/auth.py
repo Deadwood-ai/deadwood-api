5c5655ebd9df184460a1c240bc274c1cd06e6b45
JesJehle <jesvajnajehle@gmail.com>
2024-10-10 13:57:40 +0200

    formatter

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -7,17 +7,18 @@ from ..supabase import login as supabase_login
 
 router = APIRouter()
 
-@router.post("/token")
+
+@router.post('/token')
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    """
-    This login function is not used in the Forntend. 
+	"""
+	This login function is not used in the Forntend.
 
-    It is needed for the documentation, to actually test the API and for all 
-    use-cases where a Python script is uploading data to the API.
-    
-    """
-    # use the supabase login to verify the token there
-    response = supabase_login(form_data.username, form_data.password)
+	It is needed for the documentation, to actually test the API and for all
+	use-cases where a Python script is uploading data to the API.
 
-    # return the response as FastAPI needs it
-    return {"access_token": response.session.access_token, "token_type": "bearer"}
+	"""
+	# use the supabase login to verify the token there
+	response = supabase_login(form_data.username, form_data.password)
+
+	# return the response as FastAPI needs it
+	return {'access_token': response.session.access_token, 'token_type': 'bearer'}
