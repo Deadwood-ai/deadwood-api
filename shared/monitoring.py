c2837fdf41e2fe171c8f93a8422564c8aa86e5cc
Mirko Mälicke <mirko@hydrocode.de>
2024-12-06 13:05:49 +0100

    fix typos

--- shared/monitoring.py
+++ shared/monitoring.py
@@ -3,11 +3,11 @@ import logfire
 
 # get the dev mode from the environment
 dev_mode = os.environ.get('DEV_MODE', 'false').lower() == 'true'
-# first thing we to is configure logfire
+# first thing we do is configure logfire
 logfire.configure(environment='development' if dev_mode else 'production')
 
 # uncomment here to disable system-metrics logging
-# I am not enirely sure, what exactly this measures inside a docker container
+# I am not entirely sure, what exactly this measures inside a docker container
 logfire.instrument_system_metrics({
     'process.runtime.memory': ['rss', 'vms'],
     'process.runtime.cpu.utilization': None,
